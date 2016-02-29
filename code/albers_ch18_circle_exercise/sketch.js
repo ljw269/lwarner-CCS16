dc41ab1ef972ef06016acd89ff54e66ca679918e
ljw269 <ljw269@nyu.edu>
2016-02-29 07:57:20 -0500

    week 4 homework assignment

--- code/albers_ch18_circle_exercise/sketch.js
+++ code/albers_ch18_circle_exercise/sketch.js
@@ -1,14 +1,35 @@
 function setup() {
   createCanvas(500,800);
-  colorMode(HSB); 
-  background(200,50,50)
-  
+  colorMode(HSB, 360,100,100);
+  frameRate(.5);
+
 }
 
 function draw() {
+  
+  fill(200,150,150);
+  rect(0,0,500,200);
+  //segment 1
+  
+  fill(200,75,100);
+  rect(0,200,500,200)
+  //segment 2
+  
+  fill(200,100,75);
+  rect(0,400,500,200);
+  //segment 3
+  
+  fill(200,50,50);
+  rect(0,600,500,200);
+  //segment 4
+  
   for(var x = 0; x <= width; x += 50){
     for(var y = 0; y <= height; y += 50){
-    ellipse(x, y, 50, 50);
+      noStroke()
+      fill(200,random(75,100),random(25,100))
+      ellipse(x, y, 50, 50);
+      
+      //circles
     }
   }
 }
